dc3814178274ce32339f025cdfb4892a01ae0264
Moises Martinez <moisesmartinez@MacBook-Pro.local>
2023-01-31 12:01:15 -0600

    deploy3

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const {Pool} = require('pg');
 
 const client = new Pool({
-    user: 'moisesmartinez',
-    hostname: 'localhost',
-    port: 5432,
-    database: 'my_sneakers'
+    // user: 'moisesmartinez',
+    // hostname: 'localhost',
+    // port: 5432,
+    // database: 'my_sneakers'
+    connectionString: process.env.DATABASE_URL
 })
 
 module.exports = client;
+
